fc2350a5964d8a36e5dcaf8c4f837c4dda528327
augustinmbt <augustinmbath@gmail.com>
2019-06-11 13:43:11 +0200

    homepage
    nav done, mais les liens a stop l'animation; pb de centrage du hero; footer done

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const myButtons = document.querySelectorAll(".my-button");
+const $menuItem = document.querySelector(".menu");
+
+function addOpenClass() {
+    $menuItem.classList.toggle("open");
+}
+for (let i = 0; i < myButtons.length; i++) {
+    myButtons[i].addEventListener("click", addOpenClass);
+}
+
+
+const plus = document.querySelector('.plus')
+const show = document.querySelector('.hidden')
+
+function showMore() {
+
+}
