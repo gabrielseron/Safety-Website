74b8af1e0a740c86290920a9543f486759a8e279
gabrielseron <47740013+gabrielseron@users.noreply.github.com>
2019-06-21 01:26:57 +0200

    Correct W3C's Errors + Beautify Code

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -2,16 +2,16 @@ const myButtons = document.querySelectorAll(".my-button");
 const $menuItem = document.querySelector(".menu");
 
 function addOpenClass() {
-    $menuItem.classList.toggle("open");
+  $menuItem.classList.toggle("open");
 }
 for (let i = 0; i < myButtons.length; i++) {
-    myButtons[i].addEventListener("click", addOpenClass);
+  myButtons[i].addEventListener("click", addOpenClass);
 }
 
 
 var scroll_State = 0
 var initialScroll = 0
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function() {
   scroll_State = window.scrollY;
   console.log(scroll_State);
   if (scroll_State > initialScroll) {
